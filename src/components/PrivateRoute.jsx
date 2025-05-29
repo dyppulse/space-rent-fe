05e3e227d792edc67878b26ea271498c97f877dc
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2025-05-29 21:49:10 +0300

    AUthentication with redux tool kit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
+  const [token, setToken] = useState(localStorage.getItem("token"))
+
+  // useEffect(() => {
+  //   if(localStorage.getItem("token")){
+  //     setToken(localStorage.getItem("token"))
+  //   }else{
+  //     setToken(null)
+  //   }
+  // }, [localStorage.getItem('token')])
   return token ? children : <Navigate to="/auth/login" replace />;
 };
 
--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,19 +1,56 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axiosInstance from '../../api/axiosInstance';
+import { unProtectedAxiosInstance } from '../../api/axiosInstance';
+import axios from 'axios';
 
 const initialState = {
   user: null,
   loading: false,
   error: null,
+  signUpError: null
 };
 
 export const login = createAsyncThunk(
   'auth/login',
-  async ({ email, password }) => {
-    const res = await axiosInstance.post('/login', { email, password });
-    return res.data;
+
+  async ({ email, password }, { rejectWithValue }) => {
+
+    try {
+      const response = await unProtectedAxiosInstance.post('/auth/login', {
+        email,
+        password,
+      });
+      localStorage.setItem("token", response?.data?.token)
+      return response.data;
+    } catch (err) {
+      // debugger
+      const message =
+        err.response?.data?.message || err.message || 'Login failed';
+      return rejectWithValue(message);
+    }
   }
 );
+
+export const signUp = createAsyncThunk(
+  "SIGN_UP",
+  async ({ name, email, password, phone }, { rejectWithValue }) => {
+    try {
+      const response = await unProtectedAxiosInstance.post("/auth/register", {
+        name,
+        email,
+        password,
+        phone
+      })
+      localStorage.setItem("token", response?.data?.token)
+      return response.data;
+
+    } catch (err) {
+      const message =
+        err.response?.data?.message || err.message || 'Login failed';
+      return rejectWithValue(message);
+    }
+  }
+)
 
 const authSlice = createSlice({
   name: 'auth',
@@ -32,10 +69,21 @@ const authSlice = createSlice({
       .addCase(login.fulfilled, (state, action) => {
         state.loading = false;
         state.user = action.payload.user;
+        state.error = null
       })
-      .addCase(login.rejected, (state) => {
+      .addCase(login.rejected, (state, action) => {
         state.loading = false;
-        state.error = 'Invalid credentials';
+        state.error = action.payload;
+        state.user = {}
+      })
+      .addCase(signUp.pending, (state) => {
+        state.loading = true;
+        state.signUpError = null;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.loading = false,
+          state.signUpError = null,
+          state.user = action.payload
       });
   },
 });
